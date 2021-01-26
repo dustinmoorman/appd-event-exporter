05f85792cd1a5ef25146e69d544f2b6adcdc8e20
Dustin Moorman <dustin.moorman@appdynamics.com>
2021-01-26 13:25:04 -0600

    works with saas

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 import requests
 
+apiKeyFile = open("apiKey.txt", "r")
+accountNameFile = open("accountName.txt", "r")
+apiKey = apiKeyFile.read().strip()
+accountName = accountNameFile.read().strip()
+
+print("Using Account Name: ", accountName)
+print("Using API Key: ", apiKey)
+
 url = "https://analytics.api.appdynamics.com/events/query"
-headers = {}
+headerdata = {'X-Events-API-AccountName': accountName, 'X-Events-API-Key': apiKey, 'Content-Type': 'application/vnd.appd.events+json;v=2', 'Accept': 'application/vnd.appd.events+json;v=2'}
 query = "SELECT * from browser_records"
 
-data = requests.post(url, query, headers)
+response = requests.post(url, data = query, headers = headerdata)
 
-print(data)
+print(response.text)
